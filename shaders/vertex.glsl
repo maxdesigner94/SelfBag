7433714747ddd105e08c3a8399a2b7fdf38e0cdf
maxdesigner94 <144882359+maxdesigner94@users.noreply.github.com>
2025-11-26 05:14:19 +0100

    Update vertex.glsl

--- shaders/vertex.glsl
+++ shaders/vertex.glsl
@@ -7,7 +7,7 @@ varying float vProgress;
 
 void main() {
     vUv = uv;
-    // Calcola il progresso lungo la geometria (lunghezza della curva)
+    // Calcola il progresso lungo l'asse X della geometria del tubo.
     vProgress = position.x; 
 
     gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
